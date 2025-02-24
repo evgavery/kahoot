68af0941ac5020862f32d277f2802796da9b4d48
evgenyaveryanov <evgavery@gmail.com>
2025-02-24 01:06:43 +0100

    Minor UI fixes

--- src/app/core/directives/ui/button.directive.ts
+++ src/app/core/directives/ui/button.directive.ts
@@ -1,13 +1,17 @@
-import {Directive, effect, ElementRef, input, Renderer2} from '@angular/core';
+import {Directive, effect, ElementRef, inject, input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[kahButton]'
 })
 export class ButtonDirective {
+
+  private el = inject(ElementRef);
+  private renderer = inject(Renderer2);
+
   type = input<'button' | 'submit' | 'reset'>('button');
   disabled = input<boolean>(false);
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {
+  constructor() {
     effect(() => {
       this.renderer.setAttribute(this.el.nativeElement, 'type', this.type());
     });
